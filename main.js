663b724c73552405f936807625b86b133baa1880
Vaishnawik05 <79893634+Vaishnawik05@users.noreply.github.com>
2022-05-09 05:55:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX =0;
 rightWristY =0;
+
 scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
 songStatus = "";
+song_status= "";
 
 function preload()
 {
@@ -13,26 +17,25 @@ function preload()
     song2 = loadSound("music2.mp3");
 }
 
-function setup()
-{
-canvas = createCanvas(600, 500);
-canvas.center();
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
 
-video = createCapture(VIDEO);
-video.hide();
+	video = createCapture(VIDEO);
+	video.hide();
 
-posenet = ml5.poseNet(video, modelLoaded);
-poseNet.on('pose', gotPoses);
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded(){
-   console.log('PoseNet is intialized');
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
 }
 
 function draw()
 {
 image(video, 0, 0, 600, 500);
-songStatus = music.mp3.isPlaying()
+songStatus = song1.isPlaying();
 fill("#FF0000");
     stroke("#FF0000");
 
@@ -40,12 +43,26 @@ fill("#FF0000");
   {
     circle(leftWristX, leftWristY, 20);
     song2.stop();
-  }
 
-  if(songStatus = false)
+    if(songStatus = false)
   {
     song1.play();
     document.getElementById("song_name").innerHTML = "Peter Pan Song";
+  }
+  }
+
+  song_status = song2.isPlaying();
+
+  if(scoreRightWrist >0.2)
+  {
+    circle(rightWristX, rightWristY, 20);
+    song1.stop();
+
+    if(song_status = false)
+    {
+      song2.play();
+      document.getElementById("song_name").innerHTML = "Harry Potter Theme Song";
+    }
   }
 }
 
@@ -53,8 +70,9 @@ function gotPoses(results){
     if(results.length > 0);
     {
         console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
         scoreLeftWrist = results[0].pose.keypoints[9].score;
-        console.log("scoreLeftWrist = " + scoreLeftWrist);
+        console.log("scoreRightWrist = " + scoreRightWrist +  "scoreLeftWrist = " + scoreLeftWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
